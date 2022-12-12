218413727bbb6b814c148242df2b073b82a2ad55
Doushabo <2824868962@qq.com>
2022-12-12 23:13:32 +0800

    重写Router原型的push和replace方法

--- src/router/index.js
+++ src/router/index.js
@@ -8,26 +8,65 @@ import RegiIndex from "@/pages/Register/RegiIndex";
 
 Vue.use(VueRouter)
 
+// 先把原型的push方法备份
+let originPush = VueRouter.prototype.push
+let originReplace = VueRouter.prototype.replace
+// 重写push | replace 方法
+VueRouter.prototype.push = function(location, resolve, reject) {
+  if (resolve && reject) {
+    originPush.call(this, location, resolve, reject)
+  } else {
+    originPush.call(this, location, () => {}, () => {})
+  }
+}
+VueRouter.prototype.replace = function(location, resole, reject) {
+  if (resole && reject) {
+    originReplace.call(this, location, resole, reject)
+  } else {
+    originReplace.call(this, location, () => {}, () => {})
+  }
+}
+
 const routes = [
   {
     path: '/home',
     name: 'Home',
-    component: HomeIndex
+    component: HomeIndex,
+    meta: {
+      show: true
+    }
   },
   {
-    path: '/search',
+    path: '/search/:keyword?',
     name: 'Search',
-    component: SearIndex
+    component: SearIndex,
+    meta: {
+      show: true
+    },
+    // props: true
+    // 常用用函数写法
+    // props: ($route) => {
+    //   return {
+    //     keyword: $route.params.keyword,
+    //     k: $route.query.k
+    //   }
+    // }
   },
   {
     path: '/login',
     name: 'Login',
-    component: LoginIndex
+    component: LoginIndex,
+    meta: {
+      show: false
+    }
   },
   {
     path: '/register',
     name: 'Register',
-    component: RegiIndex
+    component: RegiIndex,
+    meta: {
+      show: false
+    }
   },
 
   // 重定向
