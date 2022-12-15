891644a21f0db52b4238cd3cf6154fc47563bbad
BuildTools <unconfigured@null.spigotmc.org>
2022-12-15 18:21:37 +0800

    将三级联动设置成全局组件

--- src/main.js
+++ src/main.js
@@ -2,8 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 三级联动组件 注册为全局组件
+import TypeNav from "@/components/TypeNav/TypeNav";
+Vue.component(TypeNav.name, TypeNav)
+
 
 Vue.config.productionTip = false
+
 
 new Vue({
   router,
