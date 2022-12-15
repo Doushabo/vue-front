4fa2f092179e10f417ff087e72a41e7150539103
BuildTools <unconfigured@null.spigotmc.org>
2022-12-15 18:19:52 +0800

    添加各种静态组件

--- src/pages/Home/HomeIndex.vue
+++ src/pages/Home/HomeIndex.vue
@@ -1,12 +1,33 @@
 <template>
   <div>
-    我是首页
+    <!-- 需要使用三级联动Nav -->
+    <TypeNav/>
+    <ConImages/>
+    <RecIndex/>
+    <RankIndex/>
+    <LikeIndex/>
+    <FloorIndex/>
+    <BrandIndex/>
   </div>
 </template>
 
 <script>
+import ConImages from "@/pages/Home/ListContainer/ConImages";
+import RecIndex from "@/pages/Home/Recommend/RecIndex";
+import RankIndex from "@/pages/Home/Rank/RankIndex";
+import LikeIndex from "@/pages/Home/Like/LikeIndex";
+import FloorIndex from "@/pages/Home/Floor/FloorIndex";
+import BrandIndex from "@/pages/Home/Brand/BrandIndex";
 export default {
-  name: "HomeIndex"
+  name: "HomeIndex",
+  components: {
+    ConImages,
+    RecIndex,
+    RankIndex,
+    LikeIndex,
+    FloorIndex,
+    BrandIndex
+  }
 }
 </script>
 
