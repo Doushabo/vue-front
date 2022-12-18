5fa1de5b91b8f48e63681c2c2aaf1d40b9126fb2
BuildTools <unconfigured@null.spigotmc.org>
2022-12-18 19:57:00 +0800

    加了些配置

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    proxy: {
+      "/api": {
+        // 数据来自哪台服务器 你就写哪台
+        target: "http://gmall-h5-api.atguigu.cn"
+      }
+    }
+  }
 })
