7e131ffe172456fb6abf27396e3bb4152096f3cd
BuildTools <unconfigured@null.spigotmc.org>
2022-12-21 17:55:10 +0800

    Vuex仓库

--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,15 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+// 引入各个模块的仓库
+import home from "@/store/home";
+import search from "@/store/search";
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+    // 实现Vuex仓库模块式开发存储数据
+    modules: {
+        home,
+        search
+    }
 })
