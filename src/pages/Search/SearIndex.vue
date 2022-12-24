b28e46c043c23395efdf2feb763d7f52d76ccb6a
BuildTools <unconfigured@null.spigotmc.org>
2022-12-24 20:24:05 +0800

    修改了一些逻辑

--- src/pages/Search/SearIndex.vue
+++ src/pages/Search/SearIndex.vue
@@ -1,16 +1,13 @@
 <template>
   <div>
-    <h1>这是params参数 {{$route.params.keyword}}</h1>
-    <h1>这是query参数 {{$route.query.k}}</h1>
-<!--    <h1>这是params参数 {{keyword}}</h1>-->
-<!--    <h1>这是query参数 {{k}}</h1>-->
+    <TypeNav/>
   </div>
 </template>
 
 <script>
 export default {
   name: "SearIndex",
-  // props: ['keyword', 'k']
+
 }
 </script>
 
