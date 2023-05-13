559cc8b7e40926fda6547ed8f459f2a4e0fbe78d
Doushabo <2824868962@qq.com>
2023-05-14 00:12:23 +0800

    add BannerList

--- src/store/home/index.js
+++ src/store/home/index.js
@@ -1,22 +1,33 @@
 // home模块的仓库
-import {reqCategoryList} from "@/api";
+import {reqBannerList, reqCategoryList} from "@/api";
 
 export default {
     state: {
-        categoryList: []
+        categoryList: [],
+        bannerList: []
     },
     getters: {
     },
     mutations: {
         CATEGORYLIST(state, categoryList) {
             state.categoryList = categoryList
+        },
+        BANNERLIST(state, bannerList) {
+            state.bannerList = bannerList;
         }
     },
     actions: {
         async categoryList({commit}) {
             let result = await reqCategoryList();
             if (result.code == 200) {
-                commit("CATEGORYLIST", result.data)
+                commit("CATEGORYLIST", result.data);
+            }
+        },
+        // get bannerList
+        async getBannerList({commit}) {
+            let result = await reqBannerList();
+            if (result.code == 200) {
+                commit("BANNERLIST", result.data);
             }
         }
     },
