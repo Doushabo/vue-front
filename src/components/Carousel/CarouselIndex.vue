3561fb5a17fd01895036b6557d8bdb6e214418c0
Doushabo <2824868962@qq.com>
2023-05-14 16:41:23 +0800

    create global Carousel component

---
+++ src/components/Carousel/CarouselIndex.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="swiper-container" ref="floor1Swiper">
+    <div class="swiper-wrapper">
+      <div class="swiper-slide" v-for="(carousel) in lists" :key="carousel.id">
+        <img :src="carousel.imgUrl">
+      </div>
+    </div>
+    <!-- 如果需要分页器 -->
+    <div class="swiper-pagination"></div>
+
+    <!-- 如果需要导航按钮 -->
+    <div class="swiper-button-prev"></div>
+    <div class="swiper-button-next"></div>
+  </div>
+</template>
+
+<script>
+import Swiper from "swiper";
+export default {
+  name: "CarouselIndex",
+  props: ['lists'],
+  watch: {
+    lists: {
+      immediate: true,
+      handler() {
+        // 只能监听到数据已经有了，但是v-for动态渲染结构我们还是没有办法确定
+        // 因此，还是需要nextTick
+        this.$nextTick(() => {
+          // eslint-disable-next-line no-unused-vars
+          var mySwiper = new Swiper (this.$refs.floor1Swiper, {
+            loop: true,
+            // 如果需要分页器
+            pagination: {
+              el: ".swiper-pagination",
+              clickable: true
+            },
+            // 如果需要前进后退按钮
+            navigation: {
+              nextEl: '.swiper-button-next',
+              prevEl: '.swiper-button-prev',
+            },
+            // 如果需要滚动条
+            scrollbar: '.swiper-scrollbar',
+          })
+        })
+      }
+    }
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
