461496615872e3d472e523afc77c6244a7327525
Doushabo <2824868962@qq.com>
2023-05-15 17:56:08 +0800

    add json

--- src/store/home/index.js
+++ src/store/home/index.js
@@ -2,6 +2,8 @@
 import {reqBannerList, reqCategoryList, reqFloorsList} from "@/api";
 
 export default {
+    // 开启命名空间
+    // namespaced: true,
     state: {
         categoryList: [],
         bannerList: [],
